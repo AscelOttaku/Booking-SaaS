0cbb64c31bdea51108a2d0b026e4392a18f0272c
Kanybek <zhanybek20065732@gmail.com>
2025-06-23 21:03:40 +0600

    git init

---
+++ src/main/java/kg/attractor/bookingsaas/BookingSaaSApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.bookingsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSaaSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSaaSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/bookingsaas/BookingSaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.bookingsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSaaSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
