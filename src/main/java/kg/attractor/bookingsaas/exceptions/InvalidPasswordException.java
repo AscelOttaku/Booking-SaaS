a0d1c8a92bcb7eae0fb5bc0c7abeb7cd848f66f1
Kanybek <zhanybek20065732@gmail.com>
2025-07-29 14:16:19 +0600

    exception signature changed

--- src/main/java/kg/attractor/bookingsaas/exceptions/InvalidPasswordException.java
+++ src/main/java/kg/attractor/bookingsaas/exceptions/InvalidPasswordException.java
@@ -5,16 +5,9 @@ import lombok.Getter;
 @Getter
 public class InvalidPasswordException extends RuntimeException {
     private final String messageCode;
-    private final Object[] args;
 
-    public InvalidPasswordException(String messageCode, Object[] args) {
-        super(messageCode);
-        this.messageCode = messageCode;
-        this.args = args;
-    }
     public InvalidPasswordException(String messageCode) {
         super(messageCode);
         this.messageCode = messageCode;
-        this.args = null;
     }
 }
